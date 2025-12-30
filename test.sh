8234614ecbc650830859907637260c5b00207c10
vmacp <valentin.magda@systeme-u.fr>
2025-12-30 12:22:30 +0100

    clean

--- test.sh
+++ test.sh
@@ -2,8 +2,19 @@
 set -euo pipefail
 
 shopt -s globstar nullglob
+status=0
+declare -a unclean_scripts
 for script in scripts/**/*.sh; do
     echo "=> Linting $script through shellcheck"
-    shellcheck "$script"
-    echo "   Script $script ok"
+    if shellcheck "$script"; then
+        echo "   Script $script ok"
+    else
+        status=1
+        unclean_scripts+=($script)
+        echo "::error file=${script}::Shellcheck error on file ${script}"
+    fi
 done
+if [[ status -eq 1 ]]; then
+    echo "The following scripts dont pass shellckeck: ${unclean_scripts[*]}"
+    exit 1
+fi
