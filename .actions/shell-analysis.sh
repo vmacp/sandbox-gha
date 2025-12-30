42a8d0179fccd1aff88d406c0247dac2662678b8
vmacp <valentin.magda@systeme-u.fr>
2025-12-30 13:37:39 +0100

    separate script

---
+++ .actions/shell-analysis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+shopt -s globstar nullglob
+
+rootdir="$1"
+declare -a unclean_scripts
+unclean_scripts=()
+
+for script in "$rootdir"/**/*.sh; do
+    echo "::notice file=${script}::Parsing $script through shellcheck"
+    if shellcheck "$script"; then
+        echo "::notice file=${script}::shellcheck - $script ok"
+    else
+        unclean_scripts+=("$script")
+        echo "::error file=${script}::Shellcheck error on file ${script}"
+    fi
+done
+
+if [[ ${#unclean_scripts[@]} -gt 0 ]]; then
+    echo "::error::The following scripts dont pass shellckeck: ${unclean_scripts[*]}"
+    exit 1
+fi
