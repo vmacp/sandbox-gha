9371988ab5480aa83f8dc08771210d7518587dd6
vmacp <valentin.magda@systeme-u.fr>
2025-12-30 14:41:07 +0100

    clean

--- .actions/shell-analysis.sh
+++ .actions/shell-analysis.sh
@@ -6,6 +6,10 @@ rootdir="$1"
 
 unclean_scripts=()
 
+gha_error_params='file=\(.file),line=\(.line),endLine=\(.endLine)col=\(.column),endColumn=\(.endColumn)'
+gha_error_msg='\(.level)[\(.code)]: \(.message)'
+jq_exp=".comments[] | \"${gha_error_params}::${gha_error_msg}\""
+
 for script in "$rootdir"/**/*.sh; do
     echo "::notice file=${script}::Parsing $script through shellcheck"
     if shellcheck "$script"; then
@@ -13,8 +17,7 @@ for script in "$rootdir"/**/*.sh; do
     else
         unclean_scripts+=("$script")
         shellcheck_data="$(shellcheck -f json1 "$script" || true)"
-        exp='.comments[] | "file=\(.file),line=\(.line),endLine=\(.endLine)col=\(.column),endColumn=\(.endColumn)::\(.message)"' 
-        jq -r "$exp" <<< "$shellcheck_data" | while IFS= read -r log; do
+        jq -r "$jq_exp" <<< "$shellcheck_data" | while IFS= read -r log; do
             echo "::error $log"
         done
     fi
